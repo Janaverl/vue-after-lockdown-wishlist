63401996517263d1ee27407aa52163bd77960f12
Janaverl <36476740+Janaverl@users.noreply.github.com>
2020-06-01 17:29:18 +0200

    Add: bootstrap template

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,31 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <nav class="navbar navbar-light bg-light mb-5">
+      <span class="navbar-brand">
+        After lockdown bucketlist
+      </span>
+    </nav>
+    <div class="container">
+      <bucket-list></bucket-list>
+    </div>
+    <footer class="page-footer font-small blue fixed-bottom">
+      <div class="text-center py-3">created by
+        <a href="https://www.janaverlinde.be/"> Jana Verlinde</a>
+      </div>
+    </footer>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import BucketList from './components/BucketList.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    BucketList
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/components/BucketList.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="row justify-content-center">
+    <div class="col-sm-10 col-md-8 col-md-6 col card">
+        <div class="card-header row">
+          <div class="col-sm-8"><h2><b>10 things I will do when Lockdown COVID-19 is over.</b></h2></div>
+          <div class="col-sm-4">
+              <button type="button" class="btn btn-success add-new float-right"><i class="fa fa-plus"></i> Add New</button>
+          </div>
+        </div>
+        <div class="card-body">
+          <div class="progress card-title">
+            <div class="progress-bar progress-bar-striped bg-success" style="width: 10%">1</div>
+          </div>
+          <table class="table table-bordered">
+              <thead>
+                <tr>
+                  <th style="width: 20%">name</th>
+                  <th style="width: 60%">description</th>
+                  <th style="width: 20%">Actions</th>
+                </tr>
+              </thead>
+              <tbody>
+                  <tr>
+                      <td class="font-weight-bold">example</td>
+                      <td>Mijn goede vriendin Jana uitnodigen voor een BBQ.</td>
+                      <td>
+                        <a class="edit text-warning p-3"><i class="far fa-edit"></i></a>
+                        <a class="delete text-danger p-3"><i class="far fa-trash-alt"></i></a>
+                      </td>
+                  </tr>
+                  <tr>
+                    <td><input type="text" class="form-control" name="name"></td>
+                    <td><input type="text" class="form-control" name="description"></td>
+                    <td>
+                        <a class="add text-success p-3"><i class="far fa-plus-square"></i></a>
+                        <a class="delete text-danger p-3"><i class="far fa-trash-alt"></i></a>
+                    </td>  
+                  </tr>    
+              </tbody>
+          </table>
+        </div>
+    </diV>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'BucketList'
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
