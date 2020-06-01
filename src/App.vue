1d0fe1cbcddc8b0406191b33e6229f4eed70825d
Janaverl <36476740+Janaverl@users.noreply.github.com>
2020-06-01 18:14:15 +0200

    Update: components header - content - footer

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,40 @@
 <template>
   <div id="app">
-    <nav class="navbar navbar-light bg-light mb-5">
-      <span class="navbar-brand">
-        After lockdown bucketlist
-      </span>
-    </nav>
-    <div class="container">
-      <bucket-list></bucket-list>
-    </div>
-    <footer class="page-footer font-small blue fixed-bottom">
-      <div class="text-center py-3">created by
-        <a href="https://www.janaverlinde.be/"> Jana Verlinde</a>
-      </div>
-    </footer>
+
+    <app-header 
+      :appTitle = appTitle
+    ></app-header>
+
+    <app-content></app-content>
+
+    <app-footer
+      :name = createdBy.name
+      :website = createdBy.website
+    ></app-footer>
+    
   </div>
 </template>
 
 <script>
-import BucketList from './components/BucketList.vue'
+import AppHeader from './components/AppHeader.vue'
+import AppContent from './components/AppContent.vue'
+import AppFooter from './components/AppFooter.vue'
 
 export default {
   name: 'App',
+  data: function() {
+      return {
+        appTitle: 'After lockdown bucketlist',
+        createdBy: {
+          name: 'Jana Verlinde',
+          website: 'https://www.janaverlinde.be/'
+        },
+      };
+  },
   components: {
-    BucketList
+    AppHeader,
+    AppContent,
+    AppFooter
   }
 }
 </script>
---
+++ src/components/AppContent.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container">
+    <bucketlist></bucketlist>
+  </div>
+</template>
+
+<script>
+  import Bucketlist from './bucketlist/Main.vue'
+
+  export default {
+    name: 'AppContent',
+    components: {
+      Bucketlist
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
